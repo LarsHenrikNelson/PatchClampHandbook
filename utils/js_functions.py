cc17913829f07dbd04a44210f48d5301dde7ad30
Lars Henrik Nelson <larshnelson@protonmail.com>
2025-04-29 15:32:28 -0400

    Many changes, simplified JSON files, update m_spscs

--- utils/js_functions.py
+++ utils/js_functions.py
@@ -1,21 +1,21 @@
 template_psc_callback = """
-    if rise_tau == decay_tau:
-        let rise_tau += 0.001
-    let s_r_c = sample_rate / 1000
-    let rt = Math.round(rise_tau.value * s_r_c)
-    let dt = Math.round(decay_tau.value * s_r_c)
-    len = Math.round(length.value * s_r_c)
-    let spacer = Math.round(spacer.value * s_r_c)
-    let template = np.zeros(len + spacer)
-    let t_length = new Array(arraySize).fill(0);
-    let offset = len(template) - length
-    Aprime = (td / rt) ** (rt / (rt - td))
-    let y = Array.from(x, (t_length) => (
-        amplitude
-        / Aprime
-        * ((1 - (Math.exp(-x / rt))) ** risepower * Math.exp((-x / td)))
-    )
-    template[offset:] = y
+    if (rise_tau === decay_tau) {
+        rise_tau += 0.001;
+    }
+    const s_r_c = 10
+    const rt = Math.round(rise_tau.value * s_r_c)
+    const dt = Math.round(decay_tau.value * s_r_c)
+    const len = Math.round(length.value * s_r_c)
+    const spacer = 15
+    const y = new Array(len+spacer).fill(0)
+    const t_length = Array.from({ length: len }, (_, i) => 0 + i)
+    const Aprime = (dt / rt) ** (rt / (rt - dt))
+    const temp_y = t_length.map(x => {
+        return amplitude.value / Aprime * ((1 - Math.exp(-x / rt)) ** risepower.value * Math.exp(-x / dt))
+    })
+    y.splice(spacer, temp_y.length, ...temp_y);
+    const x = Array.from({ length: len+spacer }, (_, i) => 0 + i/10)
+    source.data = { x, y }
 """
 
 view_data = """
